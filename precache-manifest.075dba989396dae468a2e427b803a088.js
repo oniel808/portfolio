007ec673480c18b930e4e90bf7476e94579b3e23
Kune-San <oniel808@gmail.com>
2020-07-10 10:11:40 +0800

    Updates

---
+++ precache-manifest.075dba989396dae468a2e427b803a088.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99cd3fb452be171ba196091f75d640d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6924aed80148da431863",
+    "url": "/portfolio/static/css/main.bc3cfdbe.chunk.css"
+  },
+  {
+    "revision": "0a5fb515c4f06d441d75",
+    "url": "/portfolio/static/js/2.151ccebb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.151ccebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6924aed80148da431863",
+    "url": "/portfolio/static/js/main.0677b7bf.chunk.js"
+  },
+  {
+    "revision": "5d96ae9e392a39d29dcf",
+    "url": "/portfolio/static/js/runtime-main.a077f93d.js"
+  },
+  {
+    "revision": "462bd259d98d98460d6767edbdcdac70",
+    "url": "/portfolio/static/media/FoobarPro-Regular.462bd259.otf"
+  },
+  {
+    "revision": "99ee5c500489d6812c5ab1c981ce99bf",
+    "url": "/portfolio/static/media/designer.99ee5c50.png"
+  },
+  {
+    "revision": "ec945afa1a5a55a91815b4cf8595038a",
+    "url": "/portfolio/static/media/koalacare.ec945afa.png"
+  },
+  {
+    "revision": "5b0991ba9437c9ede530f8e2db7f6ef8",
+    "url": "/portfolio/static/media/kunesanlogo.5b0991ba.svg"
+  },
+  {
+    "revision": "b15d81546dd2d6fad2a2b3aeb7f245e8",
+    "url": "/portfolio/static/media/lead-of-engineering.b15d8154.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f03fc0ff5791232a925cd6eb820f329a",
+    "url": "/portfolio/static/media/materialui.f03fc0ff.png"
+  },
+  {
+    "revision": "9bb533b247866d1652925aba4234dcc0",
+    "url": "/portfolio/static/media/technical-architect.9bb533b2.png"
+  },
+  {
+    "revision": "62dff354ae0be8de5a2eed05408d0708",
+    "url": "/portfolio/static/media/webDev.62dff354.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio2/precache-manifest.cd013490841ba72e54255a8488188170.js"
+  "/portfolio/precache-manifest.075dba989396dae468a2e427b803a088.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
