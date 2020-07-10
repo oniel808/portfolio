72e1a123d48f9525d4ee961cb85f7c35bae25e1d
Kune-San <oniel808@gmail.com>
2020-07-10 10:07:02 +0800

    Updates

---
+++ precache-manifest.cd013490841ba72e54255a8488188170.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2ebe1fb406f75a34961759e5705274",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "282edd8b80251c19fb3b",
+    "url": "/portfolio2/static/css/main.b8145381.chunk.css"
+  },
+  {
+    "revision": "5b8eba8e4580717586d8",
+    "url": "/portfolio2/static/js/2.438216c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio2/static/js/2.438216c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282edd8b80251c19fb3b",
+    "url": "/portfolio2/static/js/main.97d740be.chunk.js"
+  },
+  {
+    "revision": "4b059261683b5be75625",
+    "url": "/portfolio2/static/js/runtime-main.1ee5a9c2.js"
+  },
+  {
+    "revision": "462bd259d98d98460d6767edbdcdac70",
+    "url": "/portfolio2/static/media/FoobarPro-Regular.462bd259.otf"
+  },
+  {
+    "revision": "99ee5c500489d6812c5ab1c981ce99bf",
+    "url": "/portfolio2/static/media/designer.99ee5c50.png"
+  },
+  {
+    "revision": "ec945afa1a5a55a91815b4cf8595038a",
+    "url": "/portfolio2/static/media/koalacare.ec945afa.png"
+  },
+  {
+    "revision": "5b0991ba9437c9ede530f8e2db7f6ef8",
+    "url": "/portfolio2/static/media/kunesanlogo.5b0991ba.svg"
+  },
+  {
+    "revision": "b15d81546dd2d6fad2a2b3aeb7f245e8",
+    "url": "/portfolio2/static/media/lead-of-engineering.b15d8154.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f03fc0ff5791232a925cd6eb820f329a",
+    "url": "/portfolio2/static/media/materialui.f03fc0ff.png"
+  },
+  {
+    "revision": "9bb533b247866d1652925aba4234dcc0",
+    "url": "/portfolio2/static/media/technical-architect.9bb533b2.png"
+  },
+  {
+    "revision": "62dff354ae0be8de5a2eed05408d0708",
+    "url": "/portfolio2/static/media/webDev.62dff354.png"
+  }
+]);
